b23f3e0e347c4bba3388267362aa84025f420cfd
Camarederic <camarederic@gmail.com>
2026-02-27 22:52:08 +0300

    added code and styles JS & CSS

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict'
+"use strict";
+
+const navItems = document.querySelectorAll(".nav-item");
+const gallery = document.querySelector(".gallery");
+const images = document.querySelector(".images");
+
+navItems.forEach((navItem, i) => {
+  navItem.addEventListener("mousemove", (e) => {
+    images.style.transform = `translateY(-${i * 25}%)`;
+
+    const color = navItem.getAttribute("data-color");
+    gallery.style.backgroundColor = `#${color}`;
+
+    let top = e.clientY;
+    let left = e.clientX;
+
+    gallery.style.top = `${top}px`;
+    gallery.style.left = `${left}px`;
+  });
+
+  navItem.addEventListener("mouseover", () => {
+    gallery.style.transform = "translate(-50%, -50%) scale(1)";
+  });
+
+  navItem.addEventListener("mouseleave", () => {
+    gallery.style.transform = "translate(-50%, -50%) scale(0)";
+  });
+});
